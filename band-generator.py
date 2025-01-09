7661b6e25c5d855b1cda61260f81c467e507a8bd
Patience Quartey <patiencequartey@outlook.com>
2025-01-09 17:01:07 +0000

    band-generator
    started 100 days of coding with a simple band generator task.
    For this project, I utilised the topics I had learned on Day 1, specifically Strings, Variables (and good naming convention) and printing words in python

---
+++ band-generator.py
@@ -0,0 +1,13 @@
+print("Hello and welcome to 'Band Generator")
+print("Let's get you a new Band Name!!!")
+city_name = input("What City did you grow up in?: ")
+pet_name = input("What's the name of your favourite pet?: ")
+print("The name of your band is" + " " + city_name + " " + pet_name + ".That's a great name!!!")
+
+
+print("Hello and welcome to 'Band Generator")
+print("Let's get you a new Band Name!!!")
+city_name = input("What City did you grow up in?\n")
+pet_name = input("What's the name of your favourite pet?\n")
+print("The name of your band is" + " " + city_name + " " + pet_name + ".That's a great name!!!")
+
